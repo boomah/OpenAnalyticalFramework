c4dba2fac4500a20b63e11fd053ba2ea381b86c9
boomah <nickdarcy@gmail.com>
2012-05-16 19:44:56 +0100

    Move a few files about

--- modules-components/browser/gui/src/com/openaf/browser/utils/BrowserUtils.scala
+++ modules-components/browser/gui/src/com/openaf/browser/utils/BrowserUtils.scala
@@ -37,16 +37,3 @@ object BrowserUtils {
     }
   }
 }
-
-sealed trait OS {
-  def windows = true
-}
-case object OSX extends OS {
-  override def windows = false
-}
-case object WindowsXP extends OS
-case object Windows7 extends OS
-case object WindowsUnknown extends OS
-case object Linux extends OS {
-  override def windows = false
-}
