fa27eaeccb8fca909b66bb887e2e8603ec9c49ae
boomah <nickdarcy@gmail.com>
2012-12-18 21:31:22 +0000

    Start of drag and drop of fields for the table view

--- modules-libraries/table.gui/src/com/openaf/table/gui/AllFieldsArea.scala
+++ modules-libraries/table.gui/src/com/openaf/table/gui/AllFieldsArea.scala
@@ -1,10 +1,19 @@
 package com.openaf.table.gui
 
 import javafx.scene.layout.StackPane
-import javafx.scene.control.{TreeItem, TreeView}
+import javafx.scene.control.{TreeCell, TreeItem, TreeView}
 import javafx.beans.property.Property
-import com.openaf.table.api.{FieldGroup, TableData}
+import com.openaf.table.api.{Field, FieldGroup, TableData}
 import javafx.beans.value.{ObservableValue, ChangeListener}
+import javafx.util.Callback
+import javafx.event.EventHandler
+import javafx.scene.input.{ClipboardContent, TransferMode, MouseEvent}
+
+object AllFieldsArea {
+  type TreeItemType = Either[String,Field]
+}
+
+import AllFieldsArea._
 
 class AllFieldsArea(tableDataProperty:Property[TableData]) extends StackPane {
   tableDataProperty.addListener(new ChangeListener[TableData] {
@@ -13,20 +22,23 @@ class AllFieldsArea(tableDataProperty:Property[TableData]) extends StackPane {
     }
   })
 
-  private val root = new TreeItem[String]("Fields")
-  private val treeView = new TreeView[String](root)
+  private val root = new TreeItem[TreeItemType]
+  private val treeView = new TreeView[TreeItemType](root)
+  treeView.setCellFactory(new Callback[TreeView[TreeItemType],TreeCell[TreeItemType]] {
+    def call(treeView:TreeView[TreeItemType]) = new TreeItemTypeTreeCell
+  })
 
-  private def updateTreeItem(treeItem:TreeItem[String], fieldGroup:FieldGroup) {
-    treeItem.setValue(fieldGroup.groupName)
+  private def updateTreeItem(treeItem:TreeItem[TreeItemType], fieldGroup:FieldGroup) {
+    treeItem.setValue(Left(fieldGroup.groupName))
     treeItem.setExpanded(true)
     fieldGroup.children.foreach(fieldGroupOrField => {
       fieldGroupOrField match {
         case Left(fieldGroup0) => {
-          val newTreeItem = new TreeItem[String]
+          val newTreeItem = new TreeItem[TreeItemType]
           treeItem.getChildren.add(newTreeItem)
           updateTreeItem(newTreeItem, fieldGroup0)
         }
-        case Right(field) => treeItem.getChildren.add(new TreeItem[String](field.displayName))
+        case Right(field) => treeItem.getChildren.add(new TreeItem[TreeItemType](Right(field)))
       }
     })
   }
@@ -37,4 +49,29 @@ class AllFieldsArea(tableDataProperty:Property[TableData]) extends StackPane {
   }
 
   getChildren.addAll(treeView)
+}
+
+class TreeItemTypeTreeCell extends TreeCell[TreeItemType] {
+  override def updateItem(treeItemType:TreeItemType, empty:Boolean) {
+    super.updateItem(treeItemType, empty)
+    if (empty) {
+      setText(null)
+    } else {
+      treeItemType match {
+        case Left(fieldGroup) => setText(fieldGroup)
+        case Right(field) => {
+          setText(field.displayName)
+          setOnDragDetected(new EventHandler[MouseEvent] {
+            def handle(event:MouseEvent) {
+              val dragboard = startDragAndDrop(TransferMode.ANY :_*)
+              val clipboardContent = new ClipboardContent
+              clipboardContent.putString(field.id)
+              dragboard.setContent(clipboardContent)
+              event.consume()
+            }
+          })
+        }
+      }
+    }
+  }
 }
--- modules-libraries/table.gui/src/com/openaf/table/gui/RowHeaderFieldsArea.scala
+++ modules-libraries/table.gui/src/com/openaf/table/gui/RowHeaderFieldsArea.scala
@@ -2,8 +2,20 @@ package com.openaf.table.gui
 
 import javafx.scene.layout.StackPane
 import javafx.scene.control.Label
+import javafx.event.EventHandler
+import javafx.scene.input.DragEvent
 
 class RowHeaderFieldsArea extends StackPane {
   private val descriptionLabel = new Label("Drop Row Header Fields Here")
   getChildren.add(descriptionLabel)
+
+  setOnDragEntered(new EventHandler[DragEvent] {
+    def handle(event:DragEvent) {
+      if (event.getDragboard.hasString) {
+        val fieldID = event.getDragboard.getString
+        println("Entered Row Header Area with " + fieldID)
+      }
+      event.consume()
+    }
+  })
 }
