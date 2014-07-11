31ba9914c4af6eb0dbf079b6a4283413bfeffd32
boomah <nickdarcy@gmail.com>
2014-07-11 20:54:30 +0100

    Updated to scala 2.11.1

--- modules-components/guiServlet/impl/src/com/openaf/guiservlet/GUIServlet.scala
+++ modules-components/guiServlet/impl/src/com/openaf/guiservlet/GUIServlet.scala
@@ -1,7 +1,6 @@
 package com.openaf.guiservlet
 
 import javax.servlet.http.{HttpServletResponse, HttpServletRequest, HttpServlet}
-import xml.XML
 import ServletHelper._
 import java.io.File
 import org.eclipse.jetty.util.IO
@@ -16,23 +15,14 @@ import GUIServlet._
 class GUIServlet(serverName:String, externalURL:String, guiUpdatePort:String, servicePort:String) extends HttpServlet {
   private val programArgsConfigLine = serverName + " com.openaf.start.GUI " + guiUpdatePort + " " + servicePort
 
-  private val standardMemory = "512m"
-  private val specifiedMemory1024 = "1024m"
-
   private val scalaLibraryJAR = new File("common-bundles/scala-library.jar")
   private val scalaLibraryName = scalaLibraryJAR.getName
 
-  private val webStartNormalMemory = "openaf.jnlp"
-  private val webStartExtraMemory = "openAFExtra.jnlp"
-
   private val bootstrapperName = "bootstrapper.jar"
-  private val webStartIcon = "webstart-icon.png"
   private val configName = "config.txt"
   private val osgiStartModuleName = "osgi-module-start.jar"
   private val felixJAR = new File("start/lib/felix.jar")
   private val felixName = felixJAR.getName
-
-  private val webStartIconName = "com/openaf/guiservlet/resources/openaf.png"
 
   private val bootstrapperMainClass = "com.openaf.bootstrapper.Bootstrapper"
 
@@ -41,10 +31,7 @@ class GUIServlet(serverName:String, externalURL:String, guiUpdatePort:String, se
 
     path match {
       case "" => returnGuiPage(resp)
-      case `webStartNormalMemory` => returnJNLPFile(resp, webStartNormalMemory, standardMemory)
-      case `webStartExtraMemory` => returnJNLPFile(resp, webStartExtraMemory, specifiedMemory1024)
       case `bootstrapperName` => returnBootstrapperJAR(resp)
-      case `webStartIcon` => returnImage(webStartIconName, resp)
       case `configName` => returnConfigPage(resp)
       case `osgiStartModuleName` => returnStartModuleJAR(req, resp)
       case `felixName` => returnFelixJAR(req, resp)
@@ -54,58 +41,25 @@ class GUIServlet(serverName:String, externalURL:String, guiUpdatePort:String, se
   }
 
   private def returnGuiPage(resp:HttpServletResponse) {
-    def link(target:String) = Address + "/" + target
     resp.setContentType("text/html")
     val writer = resp.getWriter
     writer.println("""<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">""")
     val page =
-      <html xmlns="http://www.w3.org/1999/xhtml">
-        <head>
-            <meta http-equiv="content-type" content="text/html; charset=UTF-8" />
-          <title>OpenAF - {serverName}</title>
-          <style type="text/css">
-            body {{
-            background-color: white
-            }}
-          </style>
-        </head>
-        <body>
-          <h1>Launch the GUI</h1>
-          <a href={link(webStartNormalMemory)}>Launch via web start</a><br/>
-          <a href={link(webStartExtraMemory)}>Launch via web start with extra memory</a>
-        </body>
-      </html>
-
-    XML.write(resp.getWriter, page, "UTF-8", true, null)
-  }
-
-  private def returnJNLPFile(resp:HttpServletResponse, jnlpName:String, memory:String) {
-    resp.setContentType("application/x-java-jnlp-file")
-    val jnlp =
-      <jnlp spec='1.0+' codebase={externalURL + Address + "/"} href={jnlpName}>
-        <information>
-          <title>OpenAF - {serverName}</title>
-          <vendor>OpenAF.com</vendor>
-            <homepage href='http://www.openaf.com'/>
-          <description>OpenAF - {serverName + (if (standardMemory != memory) memory else "")}</description>
-            <icon href={webStartIcon}/>
-            <offline-allowed/>
-          <shortcut><menu submenu="OpenAF"/></shortcut>
-        </information>
-        <security>
-            <all-permissions/>
-        </security>
-        <resources>
-            <j2se version='1.6+' max-heap-size={memory}/>
-            <jar href={bootstrapperName}/>
-        </resources>
-        <application-desc main-class="com.openaf.bootstrapper.Bootstrapper">
-          <argument>{externalURL}</argument>
-        </application-desc>
-          <update check='always' policy='always'/>
-      </jnlp>
-
-    XML.write(resp.getWriter, jnlp, "UTF-8", true, null)
+      s"""<html xmlns="http://www.w3.org/1999/xhtml">
+         |  <head>
+         |    <meta http-equiv="content-type" content="text/html; charset=UTF-8" />
+         |    <title>OpenAF - $serverName</title>
+         |    <style type="text/css">
+         |      body {{
+         |      background-color: white
+         |      }}
+         |    </style>
+         |  </head>
+         |  <body>
+         |    <h1>Launch the GUI - TODO GENERATE EXE HERE</h1>
+         |  </body>
+         |</html>""".stripMargin
+    resp.getWriter.println(page)
   }
 
   private def returnBootstrapperJAR(resp:HttpServletResponse) {
