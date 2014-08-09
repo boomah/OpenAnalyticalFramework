5dd53a0109b494c1cd45b981abaf8d711f7b8bef
boomah <nickdarcy@gmail.com>
2014-08-09 10:34:44 +0100

    Added a bit more test data

--- modules-components/sport/impl/src/com/openaf/sport/GoalsTableDataSource.scala
+++ modules-components/sport/impl/src/com/openaf/sport/GoalsTableDataSource.scala
@@ -14,7 +14,16 @@ class GoalsTableDataSource extends TableDataSource {
 
   val data:Array[Array[Any]] = Array(
     Array("Ba", 54, "Newcastle United", "Tottenham Hotspur", "Sports Direct Arena", "18Aug12", "17:30", "Barclays Premier League"),
-    Array("Defoe", 76, "Tottenham Hotspur", "Newcastle United", "Sports Direct Arena", "18Aug12", "17:30", "Barclays Premier League")
+    Array("Defoe", 76, "Tottenham Hotspur", "Newcastle United", "Sports Direct Arena", "18Aug12", "17:30", "Barclays Premier League"),
+    Array("Ben Arfa", 80, "Newcastle United", "Tottenham Hotspur", "Sports Direct Arena", "18Aug12", "17:30", "Barclays Premier League"),
+    Array("Nolan", 40, "West Ham United", "Aston Villa", "Upton Park", "18Aug12", "15:00", "Barclays Premier League"),
+    Array("Tevez", 40, "Manchester City", "Southampton", "Etihad Stadium", "19Aug12", "16:00", "Barclays Premier League"),
+    Array("Lambert", 59, "Southampton", "Manchester City", "Etihad Stadium", "19Aug12", "16:00", "Barclays Premier League"),
+    Array("S Davis", 68, "Southampton", "Manchester City", "Etihad Stadium", "19Aug12", "16:00", "Barclays Premier League"),
+    Array("Dzeko", 72, "Manchester City", "Southampton", "Etihad Stadium", "19Aug12", "16:00", "Barclays Premier League"),
+    Array("Nasri", 80, "Manchester City", "Southampton", "Etihad Stadium", "19Aug12", "16:00", "Barclays Premier League"),
+    Array("Ivanovic", 2, "Chelsea", "Wigan Athletic", "The DW Stadium", "19Aug12", "13:30", "Barclays Premier League"),
+    Array("Lampard", 7, "Chelsea", "Wigan Athletic", "The DW Stadium", "19Aug12", "13:30", "Barclays Premier League")
   )
   val fieldIDs:Array[FieldID] = Array(PlayerField.id, TimeField.id, TeamField.id, OppositionTeamField.id, VenueField.id,
     DateField.id, KickOffTimeField.id, CompetitionField.id)
