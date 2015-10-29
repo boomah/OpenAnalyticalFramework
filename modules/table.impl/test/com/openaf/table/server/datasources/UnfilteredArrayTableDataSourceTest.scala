4f72969d82c7a012e4e007bd4eb78aabf5404785
boomah <nickdarcy@gmail.com>
2015-10-29 19:38:45 +0000

    Added a test for Average combiner with filter

--- modules/table.impl/test/com/openaf/table/server/datasources/UnfilteredArrayTableDataSourceTest.scala
+++ modules/table.impl/test/com/openaf/table/server/datasources/UnfilteredArrayTableDataSourceTest.scala
@@ -418,5 +418,27 @@ class UnfilteredArrayTableDataSourceTest extends FunSuite {
     check(tableState, expectedRowHeaderValues, expectedColHeaders, expectedData, expectedFieldValues, expectedValueLookUp)
   }
 
-  // TODO - add another test that checks Average with filters
+  test("1 row, 1 measure, 0 column, 1 filter, average") {
+    val nameField = NameField.withFilter(RejectFilter(Set(Nick))).withKey(FilterFieldKey(0))
+    val scoreField = ScoreField.withKey(ColumnHeaderFieldKey(0)).withCombinerType(Average)
+    val tableState = TableState.Blank.withRowHeaderFields(List(GenderField))
+      .withColumnHeaderLayout(ColumnHeaderLayout(scoreField)).withFilterFields(List(nameField))
+
+    val expectedRowHeaderValues = Set(List(1), List(2))
+    val ch = List(0,0)
+    val expectedColHeaders = Set(List(0,0))
+    val expectedData = Map(
+      p(1)(ch) -> 60,
+      p(2)(ch) -> 82
+    )
+    val expectedValueLookUp = Map(
+      NameField.id -> List(NameField.id, Rosie, Laura, Josie, Nick, Paul, Ally),
+      GenderField.id -> List(GenderField.id, F, M),
+      ScoreField.id -> List(scoreField.id)
+    )
+    val expectedFieldValues = genderFieldValues(GenderField.withKey(RowHeaderFieldKey(0))) ++
+      scoreFieldValues(scoreField) ++ nameFieldValues(nameField)
+
+    check(tableState, expectedRowHeaderValues, expectedColHeaders, expectedData, expectedFieldValues, expectedValueLookUp)
+  }
 }
